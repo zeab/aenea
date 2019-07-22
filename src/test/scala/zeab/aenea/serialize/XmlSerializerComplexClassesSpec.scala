073b05813c27a081a627311f3f3f2f7f2aadffb0
kevin downs <pyrosrppltoo@gmail.com>
2019-07-21 23:17:58 -0700

    Update XmlSerializerComplexClassesSpec.scala

--- src/test/scala/zeab/aenea/serialize/XmlSerializerComplexClassesSpec.scala
+++ src/test/scala/zeab/aenea/serialize/XmlSerializerComplexClassesSpec.scala
@@ -14,7 +14,7 @@ class XmlSerializerComplexClassesSpec extends FunSuite {
     val person: Person = Person("bob", "warlock", 9, 87.3, None, Some(mount), backpack, Vector("Hogwarts", "Yavin 4"))
     val obj: Person = person
     val serializedXml: Either[Throwable, String] = obj.asXml
-    val expectedXml: String = "<person><name>bob</name><class>warlock</class><level>9</level><health>87.3</health><soulStone/><horse><name>daisy</name><speed>1.4</speed><backpack/></horse><backpack><item><name>sword</name><type>attack</type></item><item><name>shield</name><type>defense</type></item></backpack><previousDestinations>Hogwarts</previousDestinations><previousDestinations>Yavin 4</previousDestinations></person>"
+    val expectedXml: String = "<person><name>bob</name><class>warlock</class><level>9</level><health>87.3</health><soulStone/><mount><horse><name>daisy</name><speed>1.4</speed><backpack/></horse></mount><backpack><item><name>sword</name><type>attack</type></item><item><name>shield</name><type>defense</type></item></backpack><previousDestinations>Hogwarts</previousDestinations><previousDestinations>Yavin 4</previousDestinations></person>"
     assert {
       serializedXml match {
         case Right(xml) => xml == expectedXml
